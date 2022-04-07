6cfc5794521fab05088b1d47c07d60c71ba516e9
shamsullah shamsi <shamsullah07073@gmail.com>
2022-04-07 12:30:33 +0430

    App.js is edited

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import React from "react";
+import Layout from "./container/Layout/Layout";
+import Form from "./container/Form/Form";
 
 function App() {
+
   return (
     <div>
-      react
+      <Layout>
+          <Form />
+      </Layout>
     </div>
   );
 }
