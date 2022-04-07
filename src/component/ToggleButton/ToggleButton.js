38cfb1780364ae342044535bf608b8655b5fae99
shamsullah shamsi <shamsullah07073@gmail.com>
2022-04-07 12:16:19 +0430

    Toggle Button is added to component folder

---
+++ src/component/ToggleButton/ToggleButton.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./ToggleButton.css";
+
+
+const toggleButton = () => {
+    return (
+        <div className="ToogleButton">
+            <div className="ToogleButton-Bars"></div>
+            <div className="ToogleButton-Bars"></div>
+            <div className="ToogleButton-Bars"></div>
+        </div>
+    )
+};
+
+
+export default toggleButton;
