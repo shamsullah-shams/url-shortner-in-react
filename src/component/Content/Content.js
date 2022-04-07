7b2fa2013710ebda5de6322e8df0378eae4e7bad
shamsullah shamsi <shamsullah07073@gmail.com>
2022-04-07 11:59:44 +0430

    Main Content is Added to the Component folder

---
+++ src/component/Content/Content.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Content.css";
+
+const content = () => {
+    return (
+        <center className="Content">
+            <h1>Create Short Url</h1> 
+                <strong>
+                    <p>
+                        This service is provided to make easy and short urls
+                    </p>
+                </strong>
+            <strong>
+                @copy shamsullah
+            </strong>
+        </center>
+    )
+};
+
+
+export default content;
