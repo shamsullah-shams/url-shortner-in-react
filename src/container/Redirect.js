a8d61291452fad79dddd98347689350d406e25e5
shamsullah-shams <shamsullah07073@gmail.com>
2022-04-28 13:47:52 +0430

    folder structure

--- src/container/Redirect.js
+++ src/container/Redirect.js
@@ -1,6 +1,6 @@
 import React , { Component } from "react";
 import axios from "axios";
-import NotFound from "./notFound";
+import NotFound from "../notFound";
 
 
 
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter , Route , Routes } from "react-router-dom";
-import Redirect from './Redirect';
+import Redirect from './container/Redirect';
 import NotFound from "./notFound";
 
 
